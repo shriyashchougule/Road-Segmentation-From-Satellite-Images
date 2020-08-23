c75665a40561d249d5c1b58e4ad8fd5e74c5f00e
shriyashchougule <shriyash.chougule@gmail.com>
2020-08-23 20:09:35 +0530

    Add files via upload
    Updated file for 224x244 subimage crops

--- generate_dataset_using_subimages_v2.py
+++ generate_dataset_using_subimages_v2.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import random
 import h5py
+import sys
 
 def load_images_from_folder(folder):
     images = []
@@ -12,16 +13,17 @@ def load_images_from_folder(folder):
             images.append(img)
     return image
 
-def subimages_croped_from_given_images(datasetPaths, subimage_width=256, subimage_height=256, stride = 128):
+def subimages_croped_from_given_images(datasetPaths, subimage_width=224, subimage_height=224, stride = 224):
     image_list = list()
     mask_list = list()
     ignored = 0
     potentialImages = 0
     emptyImages = 0
 
-    hf_images = h5py.File('images.h5', 'w')
-    hf_masks = h5py.File('masks.h5', 'w')
-
+    #hf_images = h5py.File('TrainImages.h5', 'w')
+    #hf_masks = h5py.File('TrainMasks.h5', 'w')
+    hf_images = h5py.File('ValidationImages.h5', 'w')
+    hf_masks = h5py.File('ValidationMasks.h5', 'w')
     #hf_images.create_dataset('my_images', (178689, 256, 256, 3), dtype='uint8')
     #hf_masks.create_dataset('my_masks', (178689, 256, 256), dtype='uint8')
 
@@ -34,8 +36,8 @@ def subimages_croped_from_given_images(datasetPaths, subimage_width=256, subimag
             width, height, channels = image.shape
             for row in range(0, height - subimage_height, stride):
                 for col in range(0, width - subimage_width, stride):
-                    subimage = np.array(image[row:row+256, col:col+256])
-                    submask = np.array(mask[row:row+256, col:col+256])
+                    subimage = np.array(image[ row:row+subimage_height, col:col+subimage_width ])
+                    submask = np.array(mask[ row:row+subimage_height, col:col+subimage_width ])
 
                     if np.any(submask):
                         background, road = np.unique(submask, return_counts=True)[1]
@@ -47,19 +49,21 @@ def subimages_croped_from_given_images(datasetPaths, subimage_width=256, subimag
                         if np.sum(np.all(subimage==255, axis=2))/(subimage_width*subimage_height) > 0.2:
                             ignored += 1
                             continue
-                        hf_images.create_dataset(name = str(index), data = subimage, shape = (256,256,3), compression="gzip", compression_opts=9)
-                        hf_masks.create_dataset(name = str(index), data = submask, shape = (256,256), compression="gzip", compression_opts=9)
+                        potentialImages += 3
+
+                        hf_images.create_dataset(name = str(index), data = subimage, shape = (subimage_width,subimage_height,3), compression="gzip", compression_opts=9)
+                        hf_masks.create_dataset(name = str(index), data = submask, shape = (subimage_width,subimage_height), compression="gzip", compression_opts=9)
                         index += 1
                         # vertical flip
-                        hf_images.create_dataset(name = str(index), data = cv2.flip(subimage, 0), shape = (256,256,3), compression="gzip", compression_opts=9)
-                        hf_masks.create_dataset(name = str(index), data = cv2.flip(submask, 0), shape = (256,256), compression="gzip", compression_opts=9)
+                        hf_images.create_dataset(name = str(index), data = cv2.flip(subimage, 0), shape = (subimage_width,subimage_height,3), compression="gzip", compression_opts=9)
+                        hf_masks.create_dataset(name = str(index), data = cv2.flip(submask, 0), shape = (subimage_width,subimage_height), compression="gzip", compression_opts=9)
                         index += 1
                         # horizontal flip
-                        hf_images.create_dataset(name = str(index), data = cv2.flip(subimage, 1), shape = (256,256,3), compression="gzip", compression_opts=9)
-                        hf_masks.create_dataset(name = str(index), data = cv2.flip(submask, 1), shape = (256,256), compression="gzip", compression_opts=9)
+                        hf_images.create_dataset(name = str(index), data = cv2.flip(subimage, 1), shape = (subimage_width,subimage_height,3), compression="gzip", compression_opts=9)
+                        hf_masks.create_dataset(name = str(index), data = cv2.flip(submask, 1), shape = (subimage_width,subimage_height), compression="gzip", compression_opts=9)
                         index += 1
 
-                        potentialImages += 3
+
                     else:
                         emptyImages+= 1
         print("\npotential images: {}, index: {}, empty images: {},  ignored images: {}".format(potentialImages, index, emptyImages, ignored))
@@ -82,10 +86,14 @@ def get_correspoding_image_and_mask_paths(imageDir, maskDir):
 if __name__ == "__main__":
     #imageDir = "/home/calm/Pictures/images"
     #maskDir =  "/home/calm/Pictures/labels"
-    imageDir = "/home/calm/Downloads/road_segmentation_ideal/training/input"
-    maskDir =  "/home/calm/Downloads/road_segmentation_ideal/training/output"
+    trainimageDir = "/home/calm/Downloads/road_segmentation_ideal/training/input"
+    trainmaskDir =  "/home/calm/Downloads/road_segmentation_ideal/training/output"
 
-    pathMappings = get_correspoding_image_and_mask_paths(imageDir, maskDir)
+    valimageDir = "/home/calm/Downloads/road_segmentation_ideal/validation/input"
+    valmaskDir =  "/home/calm/Downloads/road_segmentation_ideal/validation/output"
+
+    #pathMappings = get_correspoding_image_and_mask_paths(trainimageDir, trainmaskDir)
+    pathMappings = get_correspoding_image_and_mask_paths(valimageDir, valmaskDir)
     subimages, submasks = subimages_croped_from_given_images(pathMappings)
     print("subimages {} submask {}".format(len(subimages), len(submasks)))
 
